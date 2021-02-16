1386274ceaa77ab5d15cc73bf312d4baaf77efc9
irazozulya <45012463+irazozulya@users.noreply.github.com>
2021-02-16 18:40:00 +0200

    First commit
    Created the word checker

---
+++ LanguageChecker/Checker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LanguageChecker
+{
+    public class Checker
+    {
+        public string str { get; set; }
+        public int n;
+        public int m;
+
+        public Checker(string str)
+        {
+            this.str = str;
+        }
+
+        public bool Check()
+        {
+            int length = str.Length;
+            if (length == 0)
+            {
+                n = m = 0;
+                return true;
+            }
+            else
+            {
+                int countN = 0;
+                while (str[countN] == '0')
+                {
+                    countN++;
+                    if(countN == length)
+                    {
+                        n = countN;
+                        m = 0;
+                        return true;
+                    }
+                }
+                n = countN;
+                int countM = 0;
+                for(int i = countN; i < length; i++)
+                {
+                    if (str[i] != '1' || i + 1 == length)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        i++;
+                        if (str[i] == '0')
+                        {
+                            countM++;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                m = countM;
+                return true;
+            }
+        }
+    }
+}
---
+++ TSLA_lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LanguageChecker;
+
+namespace TSLA_lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter your word: ");
+            string str = Console.ReadLine();
+
+            Checker checker = new Checker(str);
+            while(true)
+            {
+                if (checker.Check())
+                {
+                    Console.WriteLine("This word belongs to the language!\nN: {0}; M: {1}\n", checker.n, checker.m);
+                }
+                else
+                {
+                    Console.WriteLine("This word doesn't belong to the language!\n");
+                }
+
+                Console.Write("____________________________\n\nEnter another word to check: ");
+                str = Console.ReadLine();
+                checker.str = str;
+            }
+        }
+    }
+}
